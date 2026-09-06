fbba36670a398eeb9df70bcfaff0c62f1f2861e6
heidenreich20 <heidenreich.dev@gmail.com>
2026-09-06 01:09:46 -0300

    added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    Sitemap({
+      hostname: 'https://heidenreich-dev.vercel.app/',
+    })
   ],
 })
